4e146a05c015ba21bf3e5125901d054583b3da1d
Lucas Araujo <lsa77793@gmail.com>
2024-09-22 22:54:35 -0300

    boton exportar a excel

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Mercader
+﻿using OfficeOpenXml;
+
+namespace Mercader
 {
     public partial class MainPage : ContentPage
     {
@@ -46,6 +48,54 @@
         {
             var ganancias = balance.CalcularGanancias();
             GananciasLabel.Text = $"Ganancias: {ganancias:C}";
+        }
+        private void OnExportarAExcelClicked(object sender, EventArgs e)
+        {
+            string rutaArchivo = Path.Combine(FileSystem.AppDataDirectory, "balance.xlsx");
+            ExportarBalanceAExcel(balance, rutaArchivo);
+            DisplayAlert("Exportación Completa", $"Archivo exportado a {rutaArchivo}", "OK");
+        }
+
+        public void ExportarBalanceAExcel(Balance balance, string rutaArchivo)
+        {
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Balance");
+
+                worksheet.Cells[1, 1].Value = "Encargo";
+                worksheet.Cells[1, 2].Value = "Precio";
+                worksheet.Cells[1, 3].Value = "Cantidad";
+                worksheet.Cells[1, 4].Value = "Fecha";
+
+                int row = 2;
+                foreach (var venta in balance.Ventas)
+                {
+                    worksheet.Cells[row, 1].Value = venta.Encargo.Nombre;
+                    worksheet.Cells[row, 2].Value = venta.Encargo.Precio;
+                    worksheet.Cells[row, 3].Value = venta.Cantidad;
+                    worksheet.Cells[row, 4].Value = venta.Fecha.ToString("dd/MM/yyyy");
+                    row++;
+                }
+
+                worksheet.Cells[row + 1, 1].Value = "Descripción";
+                worksheet.Cells[row + 1, 2].Value = "Monto";
+                worksheet.Cells[row + 1, 3].Value = "Fecha";
+
+                row += 2;
+                foreach (var gasto in balance.Gastos)
+                {
+                    worksheet.Cells[row, 1].Value = gasto.Descripcion;
+                    worksheet.Cells[row, 2].Value = gasto.Monto;
+                    worksheet.Cells[row, 3].Value = gasto.Fecha.ToString("dd/MM/yyyy");
+                    row++;
+                }
+
+                worksheet.Cells[row + 1, 1].Value = "Ganancias";
+                worksheet.Cells[row + 1, 2].Value = balance.CalcularGanancias();
+
+                FileInfo fileInfo = new FileInfo(rutaArchivo);
+                package.SaveAs(fileInfo);
+            }
         }
 
     }
