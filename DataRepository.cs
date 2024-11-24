8633449e6f8ee2dc4f4dc66caf458ebac754c50d
Lucas Araujo <lsa77793@gmail.com>
2024-11-24 20:30:22 -0300

    modificando DataRepository y actualizando nugets

--- DataRepository.cs
+++ DataRepository.cs
@@ -6,28 +6,86 @@ using System.Threading.Tasks;
 using SQLite;
 namespace Mercader
 {
-    internal class DataRepository
+    public class DataRepository
     {
-        private SQLiteConnection conn;
-        string dbpath;
+        private readonly SQLiteAsyncConnection _database;
 
-        public DataRepository(string dbpath)
-
+        public DataRepository(string dbPath)
         {
-            dbpath = dbpath;
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Encargo>().Wait();
+            _database.CreateTableAsync<Ventas>().Wait();
+            _database.CreateTableAsync<Gasto>().Wait();
         }
 
-        private void Init()
-
+        // Métodos para guardar datos
+        public Task<int> SaveEncargoAsync(Encargo encargo)
         {
-            if (conn != null)
-                return;
-            
-            conn = new SQLiteConnection(dbpath);
-            conn.CreateTable<Person>();
-            
-            
+            if (encargo.Id != 0)
+            {
+                return _database.UpdateAsync(encargo);
+            }
+            else
+            {
+                return _database.InsertAsync(encargo);
+            }
         }
 
+        public Task<int> SaveVentaAsync(Ventas ventas)
+        {
+            if (ventas.Id != 0)
+            {
+                return _database.UpdateAsync(ventas);
+            }
+            else
+            {
+                return _database.InsertAsync(ventas);
+            }
+        }
+
+        public Task<int> SaveGastoAsync(Gasto gasto)
+        {
+            if (gasto.Id != 0)
+            {
+                return _database.UpdateAsync(gasto);
+            }
+            else
+            {
+                return _database.InsertAsync(gasto);
+            }
+        }
+
+        // Métodos para obtener datos
+        public Task<List<Encargo>> GetEncargosAsync()
+        {
+            return _database.Table<Encargo>().ToListAsync();
+        }
+
+        public Task<List<Venta>> GetVentasAsync()
+        {
+            return _database.Table<Ventas>().ToListAsync();
+        }
+
+        public Task<List<Gasto>> GetGastosAsync()
+        {
+            return _database.Table<Gasto>().ToListAsync();
+        }
+
+        // Métodos para eliminar datos
+        public Task<int> DeleteEncargoAsync(Encargo encargo)
+        {
+            return _database.DeleteAsync(encargo);
+        }
+
+        public Task<int> DeleteVentaAsync(Venta venta)
+        {
+            return _database.DeleteAsync(venta);
+        }
+
+        public Task<int> DeleteGastoAsync(Gasto gasto)
+        {
+            return _database.DeleteAsync(gasto);
+        }
     }
+
 }
