485479afd448ca99115b70e469eaca3fa1a1b06b
Lucas Araujo <lsa77793@gmail.com>
2024-11-26 15:45:57 -0300

    Clase Encargo mejorada

--- Encargo.cs
+++ Encargo.cs
@@ -10,6 +10,15 @@ namespace Mercader
         [Column("Id")]
         public int Id { get; set; }
 
+        [Column("Precio")]
+        public decimal precio { get; set; }
+
+        [Column("Cantidad")]
+        public decimal cantidad { get; set; }
+
+        [Column("Descripcion")]
+        public string? Descripcion { get; set; }
+
         [Column("Fecha")]
         public DateTime Fecha { get; set; }
     }
