9a3fe67bd4e00bdf826a68791d17147fa746a267
Lucas Araujo <lsa77793@gmail.com>
2024-12-02 22:56:50 -0300

    Ingresando propiedades a inicializar DB en App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,11 +2,16 @@
 {
     public partial class App : Application
     {
+
+        public static string? DatabasePath { get; private set; }
         public App()
         {
             InitializeComponent();
-
+            DatabasePath = Path.Combine(FileSystem.AppDataDirectory, "mercader.db3");
             MainPage = new AppShell();
         }
+
+
     }
+
 }
