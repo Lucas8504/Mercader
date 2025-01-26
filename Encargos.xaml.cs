619e83045e1a29b2b4be1b4a81d6b86393e3bd22
Lucas Araujo <lsa77793@gmail.com>
2025-01-25 23:10:33 -0300

    Actualiza navegación y se ha añadido el método `CargarEncargos`

--- Encargos.xaml.cs
+++ Encargos.xaml.cs
@@ -5,5 +5,22 @@ public partial class Encargos : ContentPage
     public Encargos()
     {
         InitializeComponent();
+        CargarEncargos();
     }
+
+    private async void CargarEncargos()
+    {
+        try
+        {
+            var encargos = await App.DataRepo.GetEncargosAsync();
+            EncargosCollectionView.ItemsSource = encargos;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al cargar los encargos: {ex.Message}");
+        }
+    }
+
+
 }
+
