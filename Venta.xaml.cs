55a73f16011df4a08a99cd1b521f564faa75b6ff
Lucas Araujo <lsa77793@gmail.com>
2025-01-27 00:01:41 -0300

    Añadir Grid y CollectionView en Venta.xaml

--- Venta.xaml.cs
+++ Venta.xaml.cs
@@ -5,5 +5,19 @@ public partial class Venta : ContentPage
     public Venta()
     {
         InitializeComponent();
+        CargarVentas();
     }
+    private async void CargarVentas()
+    {
+        try
+        {
+            var ventas = await App.DataRepo.GetVentasAsync();
+            VentasCollectionView.ItemsSource = ventas;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al cargar los encargos: {ex.Message}");
+        }
+    }
+
 }
