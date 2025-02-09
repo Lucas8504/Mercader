719fc970acc6a4d4d4d29e539cfcb2f37f6f31ee
Lucas Araujo <lsa77793@gmail.com>
2025-02-08 23:58:53 -0300

    Eliminar inicialización y registro de rutas en AppShell

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -4,13 +4,10 @@
     {
         public AppShell()
         {
-
             InitializeComponent();
             Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
             CurrentItem = new MainPage(); // Inicializar MainPage
             Routing.RegisterRoute(nameof(Encargos), typeof(Encargos));
-
-
         }
     }
 }
