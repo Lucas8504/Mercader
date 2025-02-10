687ff50675a4d07b8fab74b00098d06c3e5102ed
Lucas Araujo <lsa77793@gmail.com>
2025-02-09 23:13:00 -0300

    mejoras en MainPage

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using SQLite;
+using Microsoft.Maui.Controls;
+using System.Globalization;
 
 namespace Mercader
 {
@@ -183,11 +185,31 @@ namespace Mercader
         private async void OnVerEncargosClicked(object sender, EventArgs e)
         {
             var navigationParameter = new Dictionary<string, object>
-                    {
-                        { "MainPage", this }
-                    };
+                        {
+                            { "MainPage", this }
+                        };
             await Shell.Current.GoToAsync(nameof(Encargos), navigationParameter);
         }
     }
 
+    public class GridLengthToDoubleConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is GridLength gridLength)
+            {
+                return gridLength.Value;
+            }
+            return 0.0;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue)
+            {
+                return new GridLength(doubleValue);
+            }
+            return GridLength.Auto;
+        }
+    }
 }
