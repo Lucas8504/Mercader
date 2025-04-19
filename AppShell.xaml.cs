a0f3233adfb4443fc1d22ec238ab8cbb89cc21f5
Lucas Araujo <lsa77793@gmail.com>
2025-04-19 20:39:34 -0300

    Mejoras en diseño y simplificación de AppShell.xaml

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -5,9 +5,8 @@
         public AppShell(ShellItem mainPage)
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
             CurrentItem = mainPage; // Inicializar MainPage
-            Routing.RegisterRoute(nameof(Encargos), typeof(Encargos));
+            
         }
     }
 }
