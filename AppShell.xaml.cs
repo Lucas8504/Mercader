781afd94691f8608406f32c5945f393c1ad4dcae
Lucas Araujo <lsa77793@gmail.com>
2025-04-19 23:09:16 -0300

    Eliminada la inicialización explícita de `CurrentItem` en `AppShell.xaml.cs`

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -5,7 +5,7 @@
         public AppShell(ShellItem mainPage)
         {
             InitializeComponent();
-            CurrentItem = mainPage; // Inicializar MainPage
+            
             
         }
     }
