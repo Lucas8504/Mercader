c989ccdeac46d9ffcc6fb9d477d5ea9050b8333a
Lucas Araujo <lsa77793@gmail.com>
2025-06-08 20:41:37 -0300

    Mejoras en la interfaz y correcciones de fuentes

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -15,7 +15,7 @@ namespace Mercader
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                    fonts.AddFont("NotoColorEmoji-Regular.ttf", "NotoColorEmoji_Regular");
+                    fonts.AddFont("NotoColorEmoji-Regular.ttf", "NotoColorEmojiRegular");
                 });
             string dbPath = Path.Combine(FileSystem.AppDataDirectory, "MercaderDB.db3");
             builder.Services.AddSingleton<DataRepository>(s => new DataRepository(dbPath));
